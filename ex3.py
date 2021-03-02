2ea7f12631ffe0c8135b8a4a5e9b2ed2d118cb81
Thinushika <35593471+Thinushika@users.noreply.github.com>
2021-03-02 21:52:49 +0530

    math
    mathematical operations and print its answers.

---
+++ ex3.py
@@ -0,0 +1,17 @@
+# Excercise 3 -- numbers and math
+X = 3
+Y = 5
+Z = X + Y # summation
+P = X - Y # substraction
+Q = X / Y # divition
+R = X * Y # multiplecation
+S = X % Y # modulus
+print("Sum of X and Y is : ",Z)
+print("Sub of X and Y is : ",P)
+print("Div of X and Y is : ",Q)
+print("Mul of X and Y is : ",R)
+print("Reminder of X and Y is : ",S)
+print(3+2+1-5+4%2-1/4+6)
+print(3+2<5-7)
+
+print('\n')
