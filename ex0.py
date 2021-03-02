2a857101b775094bf6e2dbd11840bf0b057881cc
Thinushika <35593471+Thinushika@users.noreply.github.com>
2021-03-02 21:01:24 +0530

    Add files via upload

---
+++ ex0.py
@@ -0,0 +1,5 @@
+# This is python
+# My coding first try
+# python the hard way
+# comments
+# this is the section where try no 1 to run this on the terminal
---
+++ ex1.py
@@ -0,0 +1,10 @@
+# Printing hello world
+
+print("Hello world")
+print("Hellow again")
+print("I like typing this")
+print("This is fun")
+print("Yay! Printing")
+print("I'd much rather you 'not'. ")
+print('I "said" do not touch this')
+print('\n')
