e92cd1688b6ad3137ec027fa93f8a553bf0eda3a
Thinushika <35593471+Thinushika@users.noreply.github.com>
2021-03-03 22:25:28 +0530

    inputs

---
+++ ex12.py
@@ -0,0 +1,5 @@
+# Excercise 12  --  Prompting People
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("how much do you weight? ")
+print(f"So you are {age} old, {height} tall and {weight} heavy.")
