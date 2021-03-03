7ebf12ccca4fb9547422a16414f78939faaf1e87
Thinushika <35593471+Thinushika@users.noreply.github.com>
2021-03-03 13:46:46 +0530

    printing
    one or more than one line

---
+++ ex9.py
@@ -0,0 +1,11 @@
+# Excercise 9 -- printing.....
+print('\n')
+days = "\nMon Tue Wed Thu Fri Sat Sun\n"
+months = "\nJan\nFeb\nMar\nApr\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("Here are the days : ",days)
+print("Here are the months : ",months)
+print("""
+With the three double-quotes.
+We'll be able to type as we like.
+Even 4 lines or any number of lines\n""")
