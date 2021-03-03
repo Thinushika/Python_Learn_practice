8724feb41ccde074a8b01e77d6d5d27ce171eae5
Thinushika <35593471+Thinushika@users.noreply.github.com>
2021-03-03 16:28:32 +0530

    input
    here is another way of input

---
+++ ex11_practice2.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age: "))
+name = input("Enter your name: ")
+#weight = float(input("Enter your weight: "))
+weight = input("Enter your weight: ")
+print(f"{name} you are {age} years old and you have {weight}.")
